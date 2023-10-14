c576f21e44f60c527579db9003c03af14db74b4d
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2023-10-14 11:32:56 +0300

    the bytecode now supports slices allocated to an arena

--- margarine/runtime/src/bytecode.rs
+++ margarine/runtime/src/bytecode.rs
@@ -1,3 +1,5 @@
+use sti::prelude::{Arena, Vec};
+
 macro_rules! bytecode {
     ($($opcode: literal : $name: ident ( $($field: ident : $ty: ty),* ));* ;) => {
 
@@ -10,20 +12,25 @@ macro_rules! bytecode {
         ///
         /// A bytecode instruction
         ///
-        pub enum Bytecode {
+        pub enum Bytecode<'a> {
             $($name { $($field: $ty,)* },)*
         }
 
 
-        impl Bytecode {
+        #[forbid(unreachable_patterns)]
+        impl<'a> Bytecode<'a> {
             ///
             /// Calls `Self::parse_one` with `data` and puts the result in
             /// the buffer until the iterator is exhausted
             ///
-            pub fn parse<I>(data: &mut I, buffer: &mut Vec<Bytecode>)
+            pub fn parse<I>(
+                data: &mut I, 
+                buffer: &mut Vec<Bytecode<'a>>, 
+                arena: &'a sti::prelude::Arena
+            )
             where I: Iterator<Item=u8>
             {
-                while let Some(op) = Self::parse_one(data) {
+                while let Some(op) = Self::parse_one(data, arena) {
                     buffer.push(op);
                 }
             }
@@ -34,11 +41,11 @@ macro_rules! bytecode {
             /// This function will return `None` if the iterator is empty
             /// but it will panic if the bytecode instruction is corrupt
             ///
-            pub fn parse_one<I>(data: &mut I) -> Option<Bytecode>
+            pub fn parse_one<I>(data: &mut I, arena: &'a Arena) -> Option<Bytecode<'a>>
             where I: Iterator<Item=u8> 
             {
                 Some(match data.next()? {
-                    $(consts::$name => Bytecode::$name { $($field: <$ty>::parse(data)),* },)*
+                    $(consts::$name => Bytecode::$name { $($field: <$ty>::parse(data, arena)),* },)*
 
                     _ => panic!("invalid bytecode op-code"),
                 })
@@ -51,7 +58,7 @@ macro_rules! bytecode {
             /// putting the result into the `buffer`
             ///
             pub fn generate<I>(data: &mut I, buffer: &mut Vec<u8>)
-            where I: Iterator<Item=Bytecode>
+            where I: Iterator<Item=Bytecode<'a>>
             {
                 while let Some(op) = data.next() {
                     op.generate_one(buffer);
@@ -79,34 +86,143 @@ macro_rules! bytecode {
 }
 
 
-pub trait BytecodeType: Sized {
-    fn parse<T>(iter: &mut T) -> Self where T: Iterator<Item = u8>;
+bytecode!(
+    100 : Call(dst: Reg, func: u64, regs: &'a [Reg]);
+    200 : AddI(dst: Reg, lhs: Reg, rhs: Reg);
+    201 : AddF(dst: Reg, lhs: Reg, rhs: Reg);
+    202 : AddU(dst: Reg, lhs: Reg, rhs: Reg);
+    
+    203 : SubI(dst: Reg, lhs: Reg, rhs: Reg);
+    204 : SubF(dst: Reg, lhs: Reg, rhs: Reg);
+    205 : SubU(dst: Reg, lhs: Reg, rhs: Reg);
+    
+    206 : MulI(dst: Reg, lhs: Reg, rhs: Reg);
+    207 : MulF(dst: Reg, lhs: Reg, rhs: Reg);
+    208 : MulU(dst: Reg, lhs: Reg, rhs: Reg);
+    
+    209 : DivI(dst: Reg, lhs: Reg, rhs: Reg);
+    210 : DivF(dst: Reg, lhs: Reg, rhs: Reg);
+    211 : DivU(dst: Reg, lhs: Reg, rhs: Reg);
+    
+    212 : RemI(dst: Reg, lhs: Reg, rhs: Reg);
+    213 : RemF(dst: Reg, lhs: Reg, rhs: Reg);
+    214 : RemU(dst: Reg, lhs: Reg, rhs: Reg);
+);
+
+
+pub trait BytecodeType<'a>: Sized {
+    type Output;
+    fn parse(iter: &mut impl Iterator<Item=u8>, arena: &'a Arena) -> Self::Output;
     fn generate(&self, buffer: &mut Vec<u8>); 
 }
-
-
-bytecode!(
-    100 : AddI(dst: Reg, lhs: Reg, rhs: Reg);
-    101 : AddF(dst: Reg, lhs: Reg, rhs: Reg);
-    102 : AddU(dst: Reg, lhs: Reg, rhs: Reg);
-);
 
 
 #[derive(Debug, Clone, Copy)]
 pub struct Reg(pub u8);
 
-impl BytecodeType for Reg {
-    #[inline(always)]
-    fn parse<T>(iter: &mut T) -> Self
-    where T: Iterator<Item = u8> {
-        let reg = iter.next().unwrap();
-        Self(reg)
+impl BytecodeType<'_> for Reg {
+    type Output = Self;
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, arena: &Arena) -> Self::Output {
+        Reg(u8::parse(iter, arena))
     }
 
-    #[inline(always)]
-    fn generate(&self, buffer: &mut Vec<u8>) 
-    {
-        buffer.push(self.0)
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        self.0.generate(buffer)
     }
 }
 
+
+impl BytecodeType<'_> for u8 {
+    type Output = Self;
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, _: &Arena) -> Self::Output {
+        iter.next().unwrap()
+    }
+    
+    #[inline(always)]
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        buffer.push(*self)
+    }
+}
+
+
+impl<'r, 'a, A: 'a, T: BytecodeType<'a, Output=A>> BytecodeType<'a> for &'r [T] {
+    type Output = &'a [A];
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, arena: &'a Arena) -> Self::Output {
+        let len = u32::parse(iter, arena);
+        let mut vec = Vec::new_in(arena);
+
+        for _ in 0..len {
+            vec.push(T::parse(iter, arena))
+        }
+
+        vec.leak()
+    }
+    
+
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        let len : u32 = self.len().try_into().expect("length of a map can't exceed a u32");
+        len.generate(buffer);
+
+        for i in self.iter() {
+            i.generate(buffer);
+        }
+    }
+}
+
+
+impl BytecodeType<'_> for u16 {
+    type Output = Self;
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, _: &Arena) -> Self::Output {
+        u16::from_le_bytes([
+            iter.next().unwrap(), iter.next().unwrap(),
+        ])
+    }
+
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        for i in self.to_le_bytes().iter() {
+            buffer.push(*i)
+        }
+    }
+}
+
+
+impl BytecodeType<'_> for u32 {
+    type Output = Self;
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, _: &Arena) -> Self::Output {
+        u32::from_le_bytes([
+            iter.next().unwrap(), iter.next().unwrap(),
+            iter.next().unwrap(), iter.next().unwrap(),
+        ])
+    }
+
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        for i in self.to_le_bytes().iter() {
+            buffer.push(*i)
+        }
+    }
+}
+
+
+impl BytecodeType<'_> for u64 {
+    type Output = Self;
+
+    fn parse(iter: &mut impl Iterator<Item=u8>, _: &Arena) -> Self::Output {
+        u64::from_le_bytes([
+            iter.next().unwrap(), iter.next().unwrap(),
+            iter.next().unwrap(), iter.next().unwrap(),
+            iter.next().unwrap(), iter.next().unwrap(),
+            iter.next().unwrap(), iter.next().unwrap(),
+        ])
+    }
+
+    fn generate(&self, buffer: &mut Vec<u8>) {
+        for i in self.to_le_bytes().iter() {
+            buffer.push(*i)
+        }
+    }
+}
