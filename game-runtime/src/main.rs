cc3e9a7a4b2739bf675fff98c96b535304b2f770
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2024-01-08 12:07:57 +0300

    compiled code is now packed into one exe

--- game-runtime/src/main.rs
+++ game-runtime/src/main.rs
@@ -1,3 +1,23 @@
+use std::{env, fs};
+
+use game_runtime::decode;
+use wasmer::{Module, Engine, NativeEngineExt, Store, Cranelift, Instance, imports};
+
 fn main() {
-    println!("Hello, world!");
+
+    let data = {
+        let file = env::current_exe().unwrap();
+        let file = fs::read(file).unwrap();
+
+        decode(&file)
+    };
+
+    let cranelift = Cranelift::new();
+    let mut store = Store::new(cranelift);
+    let module = unsafe { Module::deserialize(&store, &*data) }.unwrap();
+    let instance = Instance::new(&mut store, &module, &imports! {}).unwrap();
+    let main = instance.exports.get_function("main").unwrap();
+    let result = main.call(&mut store, &[]).unwrap();
+    println!("{result:?}");
+
 }
