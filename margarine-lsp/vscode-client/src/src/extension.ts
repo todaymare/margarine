9a9090b8a6e99dff436196f994e4f5e4a1c48d99
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2025-12-08 10:50:03 +0000

    lsp

---
+++ margarine-lsp/vscode-client/src/src/extension.ts
@@ -0,0 +1,186 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import {
+  languages,
+  workspace,
+  EventEmitter,
+  ExtensionContext,
+  window,
+  InlayHintsProvider,
+  TextDocument,
+  CancellationToken,
+  Range,
+  InlayHint,
+  TextDocumentChangeEvent,
+  ProviderResult,
+  commands,
+  WorkspaceEdit,
+  TextEdit,
+  Selection,
+  Uri,
+} from "vscode";
+
+import * as path from "path";
+
+import {
+  Disposable,
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+// type a = Parameters<>;
+
+export async function activate(context: ExtensionContext) {
+
+  const traceOutputChannel = window.createOutputChannel("Margarine Language Server trace");
+  
+  // Try to find the LSP binary in the parent directory's target folder
+  // The structure is: untitled compiler/lsp/src and untitled compiler/margarine-lsp/vscode-client
+  // So we need to go up 2 levels to get to "untitled compiler", then look in "target"
+  let command = process.env.SERVER_PATH || "margarine-lsp";
+  
+  const extensionPath = context.extensionPath;
+  const parentDir = path.join(extensionPath, "..", "..");
+  const localBinaryDebug = path.join(parentDir, "target", "debug", "margarine-lsp");
+  const localBinaryRelease = path.join(parentDir, "target", "release", "margarine-lsp");
+  
+  // Try release build first, then debug, then use PATH
+  if (process.env.SERVER_PATH) {
+    command = process.env.SERVER_PATH;
+  } else {
+    // Use require to check if files exist since fs is not available directly
+    try {
+      const fs = require("fs");
+      if (fs.existsSync(localBinaryRelease)) {
+        command = localBinaryRelease;
+      } else if (fs.existsSync(localBinaryDebug)) {
+        command = localBinaryDebug;
+      }
+    } catch (e) {
+      // Fall back to PATH
+    }
+  }
+  
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: "debug",
+      },
+    },
+  };
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for Margarine documents
+    documentSelector: [{ scheme: "file", language: "margarine" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    traceOutputChannel,
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient("margarine-lsp", "Margarine language server", serverOptions, clientOptions);
+  // activateInlayHints(context);
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
+
+export function activateInlayHints(ctx: ExtensionContext) {
+  const maybeUpdater = {
+    hintsProvider: null as Disposable | null,
+    updateHintsEventEmitter: new EventEmitter<void>(),
+
+    async onConfigChange() {
+      this.dispose();
+
+      const event = this.updateHintsEventEmitter.event;
+      // this.hintsProvider = languages.registerInlayHintsProvider(
+      //   { scheme: "file", language: "nrs" },
+      //   // new (class implements InlayHintsProvider {
+      //   //   onDidChangeInlayHints = event;
+      //   //   resolveInlayHint(hint: InlayHint, token: CancellationToken): ProviderResult<InlayHint> {
+      //   //     const ret = {
+      //   //       label: hint.label,
+      //   //       ...hint,
+      //   //     };
+      //   //     return ret;
+      //   //   }
+      //   //   async provideInlayHints(
+      //   //     document: TextDocument,
+      //   //     range: Range,
+      //   //     token: CancellationToken
+      //   //   ): Promise<InlayHint[]> {
+      //   //     const hints = (await client
+      //   //       .sendRequest("custom/inlay_hint", { path: document.uri.toString() })
+      //   //       .catch(err => null)) as [number, number, string][];
+      //   //     if (hints == null) {
+      //   //       return [];
+      //   //     } else {
+      //   //       return hints.map(item => {
+      //   //         const [start, end, label] = item;
+      //   //         let startPosition = document.positionAt(start);
+      //   //         let endPosition = document.positionAt(end);
+      //   //         return {
+      //   //           position: endPosition,
+      //   //           paddingLeft: true,
+      //   //           label: [
+      //   //             {
+      //   //               value: `${label}`,
+      //   //               // location: {
+      //   //               //   uri: document.uri,
+      //   //               //   range: new Range(1, 0, 1, 0)
+      //   //               // }
+      //   //               command: {
+      //   //                 title: "hello world",
+      //   //                 command: "helloworld.helloWorld",
+      //   //                 arguments: [document.uri],
+      //   //               },
+      //   //             },
+      //   //           ],
+      //   //         };
+      //   //       });
+      //   //     }
+      //   //   }
+      //   // })()
+      // );
+    },
+
+    onDidChangeTextDocument({ contentChanges, document }: TextDocumentChangeEvent) {
+      // debugger
+      // this.updateHintsEventEmitter.fire();
+    },
+
+    dispose() {
+      this.hintsProvider?.dispose();
+      this.hintsProvider = null;
+      this.updateHintsEventEmitter.dispose();
+    },
+  };
+
+  workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
+  workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
+
+  maybeUpdater.onConfigChange().catch(console.error);
+}
